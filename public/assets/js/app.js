ef4692f63886edbe55571c40f14bc6bed9ab990f
Lawrence <l.rush7@gmail.com>
2019-10-27 19:14:55 -0700

    Trying to make heroku work

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,15 @@
 var express = require("express");
+var mysql = require("mysql")
  //Set Up
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+//Heroku Stuff
+var connection 
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+  
 //Parse utility. Research further
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
